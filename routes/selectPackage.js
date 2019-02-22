82b854720815b91dab723cf5c3d20bc50d2d283a
Travisse Hansen <travisseh@gmail.com>
2019-02-21 17:46:18 -0700

    add ability to select a package and update dashboard accordingly

--- routes/selectPackage.js
+++ routes/selectPackage.js
@@ -1,17 +1,31 @@
 const express = require("express")
+const Package = require("../models/model").Package
 const selectPackage = express.Router()
 
 selectPackage.get("/", function(req,res,next){
     //check if logged in
-    // check for and render different package names
-
-    res.render("selectPackage")
+    Package.find({}, function(err, foundPackages){
+        if (err) {
+            console.log(err)
+        } else {
+            res.render("selectPackage", {packages: foundPackages})
+        }
+    })
 })
 
 selectPackage.post("/", function(req, res, next){
     //return modal that asks for the product number from their receipt - if it hasn't been entered yet
     //flash success message
-    res.redirect("/dashboard")
+    const package_id = req.body.package_id
+    Package.updateMany({active: false}, function(err, result){
+        if (err) {
+            console.log(err)
+        } else {
+            Package.updateOne({_id: package_id}, {active: true}, function(err, raw){
+                res.redirect("/dashboard")
+            })
+        }
+    })
 })
 
 
