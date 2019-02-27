58f1d42b495b0719de02e7ff1be6e2a4c52cbb42
Travisse Hansen <travisseh@gmail.com>
2019-02-26 19:32:40 -0700

    accounted for when low reps are null in the displaySets ejs logic

--- modules/test.js
+++ modules/test.js
@@ -1,24 +1,2 @@
 //CALLBACKS TEST
 
-const templateSets = [1,2,3,4]
-const lastSets = [1,2,3,4]
-
-
-
-function checkSets (templateSets, lastSets) {
-    const array = []
-    lastSets.forEach(function(set, i){
-       if (set.reps === templateSets[i].high) {
-           array.push(0)
-       } else {
-           array.push(1)
-       }
-    })
-    if (sumArray(array) === 0) {
-        return true
-    } else {
-        return false
-    }
-}
-
-checkSets(templateSets,lastSets)
