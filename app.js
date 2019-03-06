020f0af9ce18117dbbc95230e019a9bc4822408c
Travisse Hansen <travisseh@gmail.com>
2019-03-06 16:24:25 -0700

    same as last commit just fixing the db variable for prod use

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ const macroCalc = require("./routes/macroCalc")
 const workout = require("./routes/workout")
 
 // DB CONNECTION
-mongoose.connect("mongodb://localhost:27017/Kino1", {useNewUrlParser: true})
+// mongoose.connect("mongodb://localhost:27017/Kino1", {useNewUrlParser: true})
 
-// mongoose.connect("mongodb+srv://admin-travisse:test123@cluster0-vd7zd.mongodb.net/Kino1", {useNewUrlParser: true})
+mongoose.connect("mongodb+srv://admin-travisse:test123@cluster0-vd7zd.mongodb.net/Kino1", {useNewUrlParser: true})
 
 // seedPackages()
 
