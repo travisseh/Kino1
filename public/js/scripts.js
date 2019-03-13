c55e9d1ae648a54884f73de42f92cbb07af00afc
Travisse Hansen <travisseh@gmail.com>
2019-03-13 11:45:46 -0600

    added jquery to the file, and ability to only show the note if you click add note - still need to figure out how to be able to remove the note

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,3 +8,28 @@
 //   }
 // }
 
+$(document).ready(function () {
+
+
+$(".addNote").on("click", function (e) { 
+    e.preventDefault()
+    const appendId = $(this).attr("id").substring(7)
+    const id = ("note"+appendId)
+    $(`#${id}`).attr("type", "text")
+    $(this).remove()
+    // $(this).attr("class", "removeNote")
+    // $(this).text("Remove Note")
+})
+
+// $(".removeNote").on("click", function (e) { 
+//     e.preventDefault()
+//     const appendId = $(this).attr("id").substring(7)
+//     console.log(appendId)
+//     const id = ("note"+appendId)
+//     console.log(id)
+//     $(`#${id}`).attr("type", "hidden")
+//     $(this).attr("class", "addNote")
+//     $(this).text("Add Note")
+// })
+
+})
