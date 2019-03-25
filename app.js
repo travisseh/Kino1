a1897d97969ac65ca00dd1dc003c1766f25e687b
Travisse Hansen <travisseh@gmail.com>
2019-03-25 11:16:47 -0600

    partially added flash messages but am running into issues with starting the server so have to go back a few commits

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express")
 const bodyParser = require("body-parser");
 const ejs = require("ejs")
 const app = express()
-const port = 8080
+const port = 3000
 const path = require("path")
 const flash = require('connect-flash')
 
--- middleware/index.js
+++ middleware/index.js
@@ -4,6 +4,7 @@ function isLoggedIn(req, res, next){
     if (req.isAuthenticated()){
         return next()
     }
+    // req.flash("error", "You need to be logged in")
     res.redirect("/")
 }
 
