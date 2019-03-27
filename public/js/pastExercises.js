162ecada37b16037c08dfbba199ee82a628c031a
Travisse Hansen <travisseh@gmail.com>
2019-03-27 11:38:03 -0600

    fixed .get to prod environment - need to set that up somewhere though

--- public/js/pastExercises.js
+++ public/js/pastExercises.js
@@ -5,7 +5,7 @@ $(".viewPast").on("click", function(){
     const exercise = $(this).data("exercise")
     const templateExerciseId = $("input[name='templateId']")[exercise].value
 
-    $.get(`http://localhost:8080/workout/warrior_shredded/A/exercise/${templateExerciseId}`, function(data){
+    $.get(`https://kinohelper.herokuapp.com//workout/warrior_shredded/A/exercise/${templateExerciseId}`, function(data){
         //open a div and fill it with content
         $(".modal-body.past-exercises").eq(exercise).html(function(){
             return `
