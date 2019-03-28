ca69614dd3ff685de68b8ee86e79df0987076b62
Travisse Hansen <travisseh@gmail.com>
2019-03-28 14:25:34 -0600

    made small fix to the note section spacing on workout.ejs

--- public/js/pastExercises.js
+++ public/js/pastExercises.js
@@ -1,3 +1,6 @@
+const localUrl = 'http://localhost:8080'
+const prodUrl = 'https://kinohelper.herokuapp.com'
+
 $(document).ready(function () {
     
 $(".viewPast").on("click", function(){
@@ -5,12 +8,13 @@ $(".viewPast").on("click", function(){
     const exercise = $(this).data("exercise")
     const templateExerciseId = $("input[name='templateId']")[exercise].value
 
-    console.log(`https://kinohelper.herokuapp.com/workout/warrior_shredded/A/exercise/${templateExerciseId}`)
-
-    $.get(`https://kinohelper.herokuapp.com/workout/warrior_shredded/A/exercise/${templateExerciseId}`, function(data){
+    $.get(`${localUrl}/workout/warrior_shredded/A/exercise/${templateExerciseId}`, function(data){
         //open a div and fill it with content
         $(".modal-body.past-exercises").eq(exercise).html(function(){
-            return `
+            if (data === null || data === undefined || data.length === 0){
+                return '<p>No sets of this exericse have been recorded yet!</p>'
+            } else {
+                return `
                 ${data.map(function(el){
                     //make the date relative to now
                     const daysFromNow = moment(el.date).fromNow()
@@ -18,11 +22,14 @@ $(".viewPast").on("click", function(){
                     return `<h5>${daysFromNow}</h5>
                     ${el.sets.map(function(set, i){
                         //return each set
-                        return `<p>Set ${i}: ${set.reps} at ${set.weight} lbs</p>`
+                        return `<p>Set ${i + 1}: ${set.reps} at ${set.weight} lbs</p>`
                     }).join('')}
                     <br>`
                 }).join('')}
             `  
+            }
+            
+           
         })
     })
 })
