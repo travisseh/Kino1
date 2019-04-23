ff909cb58064fb9d01f5df7792632838ab1257cb
Travisse Hansen <travisseh@gmail.com>
2019-04-23 17:00:19 -0600

    history now working on fullExercise modal

--- public/js/pastExercises.js
+++ public/js/pastExercises.js
@@ -3,14 +3,64 @@ $(document).ready(function () {
 const localUrl = 'http://localhost:8080'
 const prodUrl = 'https://kinohelper.herokuapp.com'
     
+$(".viewPast2").on("click", function(){
+    //look up the last exercises
+    const exercise = $(this).data("exercise")
+    // const templateExerciseId = $("input[name='templateId']")[exercise].value
+    const templateExerciseName = $("input[name='exerciseName']")[exercise].value
+    templateExerciseName.split(' ').join('_')
+    console.log("exercise name: " + templateExerciseName)
+
+    $.get(`${localUrl}/workout/warrior_shredded/A/exercise/${templateExerciseName}`, function(data){
+        $(".past-exercises2").eq(exercise).html(function(){
+            return "loading history..."
+        })
+    })
+    .fail(function(){
+        $(".past-exercises2").eq(exercise).html(function(){
+            return "sorry there was a problem loading history - please try refreshing"
+        })
+    })
+    .done(function(data){
+        //open a div and fill it with content
+        $(".past-exercises2").eq(exercise).html(function(){
+            if (data === null || data === undefined || data.length === 0 || data[0].sets[1].weight === null){
+                return '<p>No sets of this exericse have been recorded yet!</p>'
+            } else {
+                return `
+                ${data.map(function(el){
+                    //ignore NUX generated exercise
+                    if (el.sets[1].weight != null) {
+                        //make the date relative to now
+                    const daysFromNow = moment(el.date).fromNow()
+                    //return the date
+                    return `<h6>${daysFromNow}</h6>
+                    ${el.sets.map(function(set, i){
+                        //return each set
+                        let note = ""
+                        if (set.note != "" && set.note != null && set.note != undefined){
+                            note = `, ${set.note}`
+                        }
+                        return `<p><button class="btn-list-number">${i + 1}</button>   ${set.reps} reps at ${set.weight} lbs${note}</p>`
+                    }).join('')}
+                    <div class="spacer-div"></div>`
+                    }
+                }).join('')}
+            `  
+            }
+        })
+    })
+})
+
 $(".viewPast").on("click", function(){
     //look up the last exercises
     const exercise = $(this).data("exercise")
     // const templateExerciseId = $("input[name='templateId']")[exercise].value
     const templateExerciseName = $("input[name='exerciseName']")[exercise].value
     templateExerciseName.split(' ').join('_')
+    console.log("exercise name: " + templateExerciseName)
 
-    $.get(`${prodUrl}/workout/warrior_shredded/A/exercise/${templateExerciseName}`, function(data){
+    $.get(`${localUrl}/workout/warrior_shredded/A/exercise/${templateExerciseName}`, function(data){
         $(".modal-body.past-exercises").eq(exercise).html(function(){
             return "loading history..."
         })
@@ -50,4 +100,6 @@ $(".viewPast").on("click", function(){
         })
     })
 })
+
+
 });
