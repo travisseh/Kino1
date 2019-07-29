a03dc7946b6fb1e267fa2d7239998abc4b2aa944
Travisse Hansen <travisseh@gmail.com>
2019-07-29 12:05:13 -0600

    removed testimonial to it's own file to be used later and put subscription plans in .env files so it'll work properly on prod

--- routes/subscribe.js
+++ routes/subscribe.js
@@ -7,7 +7,9 @@ router.get('/', middleware.isLoggedIn, function(req, res, next){
     const error = req.flash("error")
     const success = req.flash("success")
     const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
-    res.render("subscribe", {success: success, error: error, user:req.user, stripePublicKey: stripePublicKey, ajaxUrl: process.env.AJAX_URL})
+    const stripeMonthlyPlan = process.env.STRIPE_MONTHLY_PLAN
+    const stripeAnnualPlan = process.env.STRIPE_ANNUAL_PLAN
+    res.render("subscribe", {success: success, error: error, user:req.user, stripePublicKey: stripePublicKey, stripeMonthlyPlan: stripeMonthlyPlan, stripeAnnualPlan: stripeAnnualPlan, ajaxUrl: process.env.AJAX_URL})
 })
 
 module.exports = router
